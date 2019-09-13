3ce64f16a5d465b02f4eb948e3d0f1c27f34c7a5
notepoplove <suphot.kho@msu.ac.th>
2019-09-13 14:21:00 +0700

    three finger touch exit app

--- www/oneweb-playme-plugin.js
+++ www/oneweb-playme-plugin.js
@@ -4,10 +4,11 @@ module.exports = (function() {
   touchExitApp();
 })();
 
-function touchExitApp(){
+function touchExitApp() {
   var toucstartY1 = 0;
   var toucstartY2 = 0;
   var toucstartY3 = 0;
+  var toucstartY4 = 0;
   document.addEventListener(
     "touchstart",
     function(e) {
@@ -15,10 +16,12 @@ function touchExitApp(){
         toucstartY1 = e.touches[0].screenY;
         toucstartY2 = e.touches[1].screenY;
         toucstartY3 = e.touches[2].screenY;
+        toucstartY4 = e.touches[3].screenY;
       } else {
         toucstartY1 = 0;
         toucstartY2 = 0;
         toucstartY3 = 0;
+        toucstartY4 = 0;
       }
     },
     false
@@ -26,18 +29,21 @@ function touchExitApp(){
   document.addEventListener(
     "touchmove",
     function(e) {
-      console.log("length: " + e.changedTouches.length, e);
-      if (e.changedTouches.length == 3) {
+      if (e.changedTouches.length == 4) {
         var deltaY1 = e.changedTouches[0].screenY - toucstartY1;
         var deltaY2 = e.changedTouches[1].screenY - toucstartY2;
         var deltaY3 = e.changedTouches[2].screenY - toucstartY3;
-        console.log(
-          "Delta=> F1: " + deltaY1 + " , F2: " + deltaY2 + " , F3: " + deltaY3
-        );
-        if (deltaY1 > 250 && deltaY2 > 250 && deltaY3 > 250) {
-          if(!window.__PLAYME__&&window.__PLAYME__!=="playme"){
+        var deltaY4 = e.changedTouches[3].screenY - toucstartY4;
+        if (deltaY1 > 250 && deltaY2 > 250 && deltaY3 > 250 && deltaY4 > 250) {
+          if (!window.__PLAYME__ && window.__PLAYME__ !== "playme") {
             console.log("ExitApp Success!");
-            cordova.exec(function(){},function(){}, 'WebViewPlugin', 'exitApp', []);
+            cordova.exec(
+              function() {},
+              function() {},
+              "WebViewPlugin",
+              "exitApp",
+              []
+            );
           }
         }
       }
